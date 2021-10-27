69f4c61f4c7c0c6dc31c76bb197e6fad0d223397
Reuben Raff <reubenraff@gmail.com>
2021-10-26 22:51:16 -0400

    Update calc_accuracy.py

--- calc_accuracy.py
+++ calc_accuracy.py
@@ -2,35 +2,28 @@
 import csv
 import logging
 
-count = 0
-lengths = []
-with open("column_phones.tok","r") as source:
-    csv_reader = csv.reader(source, delimiter='\t')
-    for phone1,phone2 in csv_reader:
-        print(len(phone1), len(phone2))
-        #print("".join(phone2))
-        #lengths.append(len(phone2))
-        for char1, char2  in zip(phone1,phone2):
-            if char1 == char2:
-                pass
 
-    print(lengths)
-        #any("hi" for element in s)
+def calc_accuracy():
 
-"""
-        phone1List.append(phone1)
-        #x = len(phone1)
-        #first_col_len = len(next(zip(*csv_reader)))
-        phone1_list = sorted("".join(phone1))
+    count = 0
+    with open("column_phones.tok", "r") as source:
+        csv_reader = csv.reader(source, delimiter="\t")
+        phone1List = []
+        for phone1, phone2 in csv_reader:
+            phone1List.append(phone1)
 
-        phone1_list.remove("\t")
-        phone2_list = sorted("".join(phone2)) #this is probs redundant
+            phone1_list = sorted("".join(phone1))
+            phone1_list.remove("\t")
+            phone2_list = sorted("".join(phone2))  # this is probs redundant
 
-        #print(phone1_list == phone2_list)
-        if phone1_list == phone2_list:
-            print(phone1_list,phone2_list)
-            count +=1
-"""
-#print(phone1_list)
-#wer = len(phone1List) - count
-#print(wer)
+    
+            if phone1_list == phone2_list:
+
+                count += 1
+            wer = ((len(phone1List) - count) / len(phone1List)) * 100
+    return wer
+
+
+calc_accuracy()
+
+# 17.0

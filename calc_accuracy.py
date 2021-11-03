07afb53710c353e04ee658db9219e90c28691e68
Reuben Raff <reubenraff@gmail.com>
2021-11-03 13:27:56 -0400

    Update calc_accuracy.py

--- calc_accuracy.py
+++ calc_accuracy.py
@@ -2,23 +2,18 @@
 import csv
 import logging
 
+print("in ccheck accuracy file")
 
 def calc_accuracy():
-
     count = 0
+    print("count: ", count)
     with open("column_phones.tok", "r") as source:
         csv_reader = csv.reader(source, delimiter="\t")
         phone1List = []
         for phone1, phone2 in csv_reader:
             phone1List.append(phone1)
-            phone1_list = list("".join(phone1))
-            phone1_list.remove("\t")
-            phone1 = "".join(phone1_list)
-            if phone1  == phone2:
-                print(phone1,phone2)
+            if phone1 == phone2:
                 count += 1
-            wer = ((len(phone1List) - count) / len(phone1List)) * 100
+            wer = ((len(phone1List) - count) / len(phone1List)) * 100 #17.0
     return wer
-calc_accuracy()
-
-# 17.0
+print(calc_accuracy())

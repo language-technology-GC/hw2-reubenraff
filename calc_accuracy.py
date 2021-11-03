65e3a841ac28c96bcdcf9187e55ee1d9e738bd2d
Reuben Raff <reubenraff@gmail.com>
2021-11-03 12:34:25 -0400

    Update calc_accuracy.py
    I had to cast each 'phone' in the phone1List as a list because there is a leading \t char in the string comparision. Once I removed that in line 15 I joined the characters back into strings for comparsion. I maintained a wer of 17.0 in a string equality comparison without sorting.

--- calc_accuracy.py
+++ calc_accuracy.py
@@ -4,17 +4,21 @@ import logging
 
 
 def calc_accuracy():
+
     count = 0
     with open("column_phones.tok", "r") as source:
         csv_reader = csv.reader(source, delimiter="\t")
         phone1List = []
         for phone1, phone2 in csv_reader:
             phone1List.append(phone1)
-            phone1_list = sorted("".join(phone1))
+            phone1_list = list("".join(phone1))
             phone1_list.remove("\t")
-            phone2_list = sorted("".join(phone2))  # this is probs redundant
-            if phone1_list == phone2_list:
+            phone1 = "".join(phone1_list)
+            if phone1  == phone2:
+                print(phone1,phone2)
                 count += 1
             wer = ((len(phone1List) - count) / len(phone1List)) * 100
-    return wer #17
-calc_accuracy()
+    return wer
+print(calc_accuracy())
+
+# 17.0
